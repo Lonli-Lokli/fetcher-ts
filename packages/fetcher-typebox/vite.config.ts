11cdbd1be6eb50c6e2ddeb91ca635f68aa98761d
Lonli-Lokli <lonli.lokli@gmail.com>
2025-02-24 17:58:43 +0000

    chore(release): publish 0.8.0

--- packages/fetcher-typebox/vite.config.ts
+++ packages/fetcher-typebox/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: './dist',
+    outDir: '../../dist',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
