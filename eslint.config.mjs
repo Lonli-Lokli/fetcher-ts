cb1533859d400ee1db871ce107f00e17e97eddd8
Lonli-Lokli <lonli.lokli@gmail.com>
2026-06-08 18:12:15 +0100

    update eslint ignore list

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', '**/*.timestamp-*.mjs'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
